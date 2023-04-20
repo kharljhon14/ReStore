7798945c8f9b9d94e291d2d0399b6c86dad8b2c8
Karl <enriquezkharl14@gmail.com>
2023-04-20 12:23:05 +0800

    Error handler middleware BE

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,3 @@
-
-
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
         public ProductsController(StoreContext context)
