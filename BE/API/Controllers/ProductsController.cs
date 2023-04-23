b0111021e6e82f1e055b99cbc77ece7032f8b640
Karl <enriquezkharl14@gmail.com>
2023-04-23 09:55:37 +0800

    pagination

--- BE/API/Controllers/ProductsController.cs
+++ BE/API/Controllers/ProductsController.cs
@@ -1,33 +1,41 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
+
         public ProductsController(StoreContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(
-        string orderBy,
-        string searchTerm,
-        string brands,
-        string types)
+        public async Task<ActionResult<PageList<Product>>> GetProducts(
+            [FromQuery] ProductParams productParams
+        )
         {
             var query = _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands, types)
-            .AsQueryable();
+                .Sort(productParams.OrderBy)
+                .Search(productParams.searchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
 
-            return await query.ToListAsync();
+            var products = await PageList<Product>.ToPageList(
+                query,
+                productParams.PageNumber,
+                productParams.PageSize
+            );
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -35,7 +43,8 @@ namespace API.Controllers
         {
             var product = await _context.Products.FindAsync(id);
 
-            if (product == null) return NotFound();
+            if (product == null)
+                return NotFound();
 
             return product;
         }
